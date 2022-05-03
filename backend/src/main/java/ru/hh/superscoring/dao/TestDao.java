15402c0b94acd9af4fb24e72c9ee46253d7b67b6
Кирилл Пятница <kirill.pyatnitsa@gmail.com>
2022-05-03 16:55:07 +0300

    Getting information about the test to be taken v2

--- backend/src/main/java/ru/hh/superscoring/dao/TestDao.java
+++ backend/src/main/java/ru/hh/superscoring/dao/TestDao.java
@@ -1,17 +1,29 @@
 package ru.hh.superscoring.dao;
 
+import java.util.Optional;
 import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.transaction.annotation.Transactional;
 import ru.hh.superscoring.entity.Test;
 
 public class TestDao extends GenericDao {
 
+  private static final Logger logger = LoggerFactory.getLogger(TestDao.class);
 
   public TestDao(SessionFactory sessionFactory) {
     super(sessionFactory);
   }
 
-  public Test getTestById(Integer id) {
-    return get(Test.class, id);
+  @Transactional(readOnly = true)
+  public Optional<Test> getTestById(Integer id) {
+    Test test = get(Test.class, id);
+    if (test != null) {
+      return Optional.of(test);
+    } else {
+      logger.error("There is no record with 'id' {} in the 'test' table!!!", id);
+      return Optional.empty();
+    }
   }
 
 }
--- backend/src/main/java/ru/hh/superscoring/service/TestService.java
+++ backend/src/main/java/ru/hh/superscoring/service/TestService.java
@@ -2,7 +2,6 @@ package ru.hh.superscoring.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.transaction.annotation.Transactional;
 import ru.hh.superscoring.dao.TestDao;
 import ru.hh.superscoring.entity.Test;
 import ru.hh.superscoring.validator.Validator;
@@ -19,18 +18,8 @@ public class TestService {
     this.testDao = testDao;
   }
 
-  @Transactional
   public Optional<Test> getTestById(String id) {
     OptionalInt integerId = Validator.validateId(Test.class, id);
-    if (integerId.isPresent()) {
-      Test test = testDao.getTestById(integerId.getAsInt());
-      if (test != null) {
-        return Optional.of(test);
-      } else {
-        logger.error("There is no record with 'id' {} in the 'test' table!", id);
-      }
-    }
-    return Optional.empty();
+    return integerId.isPresent() ? testDao.getTestById(integerId.getAsInt()) : Optional.empty();
   }
-
 }
