67aec1f5481212b1925a5396652814478afd4a44
Alexander Vinichenko <vinich.yota@gmail.com>
2022-05-05 23:57:57 +0300

    SS-16 basic framework application

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import TestPage from "./pages/TestPage";
+import ResultPage from "./pages/ResultPage";
+import AuthPage from "./pages/AuthPage";
 
 function App() {
     return (
-        <div className="App">
-            Hello World!
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="/auth" element={<AuthPage/>}/>
+                <Route path="/:testId" element={<TestPage/>}/>
+                <Route path="/:testId/:completionId" element={<ResultPage/>}/>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
