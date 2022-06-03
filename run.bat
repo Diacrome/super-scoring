1d6253b18e3dfe6e4c6c4a48c486e523293a41c4
Кирилл Пятница <94162007+newvasuki-newvasuki@users.noreply.github.com>
2022-06-03 18:58:49 +0300

    SS-41 added test requests for testing the backend API (#46)
    * SS-41 added test requests for testing the backend API
    
    * SS-41 added test requests for testing the backend API v2
    
    * SS-41 added test requests for testing the backend API v3
    
    * SS-41 added test requests for testing the backend API v4
    
    * Delete restart_with_new_base.bat
    
    * Delete restart_with_new_base.sh
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md

--- run.bat
+++ run.bat
@@ -1,3 +1,16 @@
+@ECHO OFF
 cd %~dp0
 docker-compose down --volumes
+IF "%~1"=="p"  GOTO postgres
+IF "%~1"=="b"  GOTO backend
+IF "%~1"=="t"  GOTO tests
 docker-compose up --build
+:postgres
+docker-compose up --build -- postgres
+exit
+:backend
+docker-compose up --build -- postgres backend
+exit
+:tests
+docker-compose up --build -- postgres backend tests
+exit
