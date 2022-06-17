8626e90d87de7b69e308cf0ecc1a57b51ca6312a
Lilongue <lilongue@gmail.com>
2022-06-17 17:38:46 +0300

    SS-50 Import optimization after rebase on main

--- backend/src/main/java/ru/hh/superscoring/resource/TestResource.java
+++ backend/src/main/java/ru/hh/superscoring/resource/TestResource.java
@@ -6,16 +6,9 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import javax.ws.rs.DefaultValue;
-import javax.ws.rs.QueryParam;
-import ru.hh.superscoring.dto.TestDto;
-import ru.hh.superscoring.dto.TestBoardDto;
-import ru.hh.superscoring.entity.Test;
-import ru.hh.superscoring.service.AuthService;
-import ru.hh.superscoring.service.StatusService;
-import ru.hh.superscoring.service.TestService;
-
+import java.util.List;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
@@ -23,14 +16,16 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import ru.hh.superscoring.dto.StatusDto;
+import ru.hh.superscoring.dto.TestBoardDto;
 import ru.hh.superscoring.dto.TestDto;
+import ru.hh.superscoring.entity.Test;
 import ru.hh.superscoring.service.AuthService;
 import ru.hh.superscoring.service.StatusService;
 import ru.hh.superscoring.service.TestService;
-import java.util.List;
 
 @Tag(name = "Тесты", description = "API для взаимодействия с тестами")
 @Path("/test")
