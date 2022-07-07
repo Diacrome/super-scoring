905e5f96e7c286b1bd4c434d1b08d2146c5c7376
Alexander Vinichenko <vinich.yota@gmail.com>
2022-07-07 22:09:36 +0300

    SS-83 Add info about next attempt

--- frontend/src/pages/TestInfo.tsx
+++ frontend/src/pages/TestInfo.tsx
@@ -5,30 +5,55 @@ import Loader from "../components/Loader/Loader";
 import { useParams } from "react-router-dom";
 import { useTestStart } from "../hooks/useTestStart";
 import { fetchData } from "../functions/fetchData";
+import { AttemptsInfo, StartResult } from "../types/attempts";
+import { getDate } from "../functions/getDate";
 
 const TestInfo: FC = () => {
   const { testId } = useParams();
   const [testDescription, setTestDescription] =
     useState<TestDescription | null>(null);
+  const [attemptsInfo, setAttemptsInfo] = useState<AttemptsInfo | null>(null);
   const testStart = useTestStart();
 
   useEffect(() => {
     fetchData(`test/info/${testId}`).then((data) => setTestDescription(data));
   }, []);
 
-  const handleTestStart = () => testStart(testId);
+  const handleTestStart = () =>
+    testStart(testId).then((data) => {
+      setAttemptsInfo(data);
+    });
 
   if (testDescription === null) return <Loader />;
 
-  return testId === "1" || testId === "2" || testId === "3" ? (
+  if (testDescription === undefined)
+    return (
+      <div className="test">
+        <h1 className="not-exist-text">Тест не существует</h1>
+      </div>
+    );
+
+  if (attemptsInfo) {
+    return (
+      <div className="test attempts-info">
+        <div>
+          {attemptsInfo.startResult === StartResult.Spent &&
+            "Истрачены три разрешнные попытки."}
+          {attemptsInfo.startResult === StartResult.Passed &&
+            "Тест успешно пройден."}
+        </div>
+        <div>{`Следующая попытка: ${getDate(attemptsInfo.nextAttempt)}`}</div>
+      </div>
+    );
+  }
+
+  return (
     <div className="test">
       <div className="test-info-text">{testDescription.description}</div>
       <div className="btn-start">
         <Button onClick={handleTestStart}>Начать тест</Button>
       </div>
     </div>
-  ) : (
-    <div>Тест не существует</div>
   );
 };
 
