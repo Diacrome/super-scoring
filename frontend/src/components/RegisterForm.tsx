593710857bca412ede5d7c1affd8655e3d5f06e9
Alexander Vinichenko <vinich.yota@gmail.com>
2022-07-09 16:31:36 +0300

    SS-116 Add register form

---
+++ frontend/src/components/RegisterForm.tsx
@@ -0,0 +1,81 @@
+import React, { FC, FormEventHandler, useState } from "react";
+import TextInput from "./Input/TextInput";
+import Button from "./Button/Button";
+import { FormProps, RegisterParams } from "../types/login";
+import { userRegister } from "../functions/userRegister";
+
+const RegisterForm: FC<FormProps> = ({ handleMode }) => {
+  const [registerParams, setRegisterParams] = useState<RegisterParams>({
+    name: "",
+    login: "",
+    password: "",
+  });
+  const [isRegistered, setIsRegistered] = useState(false);
+
+  const handleRegister: FormEventHandler = (e) => {
+    e.preventDefault();
+    userRegister(registerParams).then(() => setIsRegistered(true));
+  };
+
+  if (isRegistered) {
+    return (
+      <>
+        <div className="register-text">Вы успешно зарегистрированы</div>
+        <Button onClick={handleMode}>Войти</Button>
+      </>
+    );
+  }
+
+  return (
+    <form className="login-form" onSubmit={handleRegister} method="post">
+      <div className="form-block">
+        <label className="input-label" htmlFor="name">
+          Имя
+        </label>
+        <TextInput
+          name="name"
+          id="name"
+          value={registerParams.name}
+          onChange={(event) =>
+            setRegisterParams({ ...registerParams, name: event.target.value })
+          }
+        />
+      </div>
+      <div className="form-block">
+        <label className="input-label" htmlFor="login">
+          Логин
+        </label>
+        <TextInput
+          name="login"
+          id="login"
+          value={registerParams.login}
+          onChange={(event) =>
+            setRegisterParams({ ...registerParams, login: event.target.value })
+          }
+        />
+      </div>
+      <div className="form-block">
+        <label className="input-label" htmlFor="password">
+          Пароль
+        </label>
+        <TextInput
+          type="password"
+          name="password"
+          id="password"
+          value={registerParams.password}
+          onChange={(event) =>
+            setRegisterParams({
+              ...registerParams,
+              password: event.target.value,
+            })
+          }
+        />
+      </div>
+      <div className="form-block">
+        <Button>Зарегистрироваться</Button>
+      </div>
+    </form>
+  );
+};
+
+export default RegisterForm;
--- frontend/src/pages/LoginScreen.tsx
+++ frontend/src/pages/LoginScreen.tsx
@@ -1,66 +1,36 @@
-import React, { FC, FormEventHandler, useState } from "react";
-import Button from "../components/Button/Button";
-import TextInput from "../components/Input/TextInput";
-import { LoginParams } from "../types/login";
-import { useFetchToken } from "../hooks/useFetchToken";
-import { userRegister } from "../functions/userRegister";
+import React, { FC, useState } from "react";
+import { LoginMode } from "../types/login";
+import AuthForm from "../components/AuthForm";
+import RegisterForm from "../components/RegisterForm";
 
 const LoginScreen: FC = () => {
-  const fetchToken = useFetchToken();
+  const [loginMode, setLoginMode] = useState<LoginMode>(LoginMode.Auth);
 
-  const [loginParams, setLoginParams] = useState<LoginParams>({
-    login: "",
-    password: "",
-  });
-
-  const handleSubmit: FormEventHandler = (e) => {
-    e.preventDefault();
-    fetchToken(loginParams);
+  const handleMode = () => {
+    switch (loginMode) {
+      case LoginMode.Auth:
+        setLoginMode(LoginMode.Register);
+        break;
+      case LoginMode.Register:
+        setLoginMode(LoginMode.Auth);
+        break;
+    }
   };
 
-  const handleRegister = () => {
-    userRegister(loginParams);
-  };
-
-  return (
-    <form className="auth-form" onSubmit={handleSubmit} method="post">
-      <div className="form-block">
-        <label className="input-label" htmlFor="login">
-          Логин
-        </label>
-        <TextInput
-          name="login"
-          id="login"
-          value={loginParams.login}
-          onChange={(event) =>
-            setLoginParams({ ...loginParams, login: event.target.value })
-          }
-        />
-      </div>
-      <div className="form-block">
-        <label className="input-label" htmlFor="password">
-          Пароль
-        </label>
-        <TextInput
-          type="password"
-          name="password"
-          id="password"
-          value={loginParams.password}
-          onChange={(event) =>
-            setLoginParams({ ...loginParams, password: event.target.value })
-          }
-        />
-      </div>
-      <div className="form-block">
-        <Button>Войти</Button>
-      </div>
-      <div className="form-block">
-        <Button type="button" onClick={handleRegister}>
-          Зарегистрироваться
-        </Button>
-      </div>
-    </form>
-  );
+  switch (loginMode) {
+    case LoginMode.Auth:
+      return (
+        <div className="login-screen">
+          <AuthForm handleMode={handleMode} />
+        </div>
+      );
+    case LoginMode.Register:
+      return (
+        <div className="login-screen">
+          <RegisterForm handleMode={handleMode} />
+        </div>
+      );
+  }
 };
 
 export default LoginScreen;
